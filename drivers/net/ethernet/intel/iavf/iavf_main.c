0109d80bdd15ef97c8d1ea97de7ce8180308f72b
Alexander Lobakin <aleksander.lobakin@intel.com>
2023-03-15 17:50:17 +0100

    iavf: switch queue stats to libie
    iavf is pretty much ready for using the generic libie stats, so drop all
    the custom code and just use generic definitions. The only thing is that
    it previously lacked the counter of Tx queue stops. It's present in the
    other drivers, so add it here as well.
    The rest is straightforward. There were two fields in the Tx stats
    struct, which didn't belong there. The first one has never been used,
    wipe it; and move the other to the queue structure. Plus move around
    a couple fields in &iavf_ring to account stats structs' alignment.
    
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>

--- drivers/net/ethernet/intel/iavf/iavf_main.c
+++ drivers/net/ethernet/intel/iavf/iavf_main.c
@@ -1581,6 +1581,7 @@ static int iavf_alloc_queues(struct iavf_adapter *adapter)
 		tx_ring->itr_setting = IAVF_ITR_TX_DEF;
 		if (adapter->flags & IAVF_FLAG_WB_ON_ITR_CAPABLE)
 			tx_ring->flags |= IAVF_TXR_FLAGS_WB_ON_ITR;
+		u64_stats_init(&tx_ring->sq_stats.syncp);
 
 		rx_ring = &adapter->rx_rings[i];
 		rx_ring->queue_index = i;
@@ -1588,6 +1589,7 @@ static int iavf_alloc_queues(struct iavf_adapter *adapter)
 		rx_ring->dev = &adapter->pdev->dev;
 		rx_ring->count = adapter->rx_desc_count;
 		rx_ring->itr_setting = IAVF_ITR_RX_DEF;
+		u64_stats_init(&rx_ring->rq_stats.syncp);
 	}
 
 	adapter->num_active_queues = num_active_queues;
